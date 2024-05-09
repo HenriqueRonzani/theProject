680d127805e0710880af50d86888f0b659c67311
HenriqueRonzani <henriquedasilvaronzani@gmail.com>
2024-05-08 23:20:03 -0300

    Test

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,6 @@
 <?php
 
 use App\Http\Controllers\AppController;
-use App\Http\Middleware\CorsMiddleware;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,14 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/getData', [AppController::class, 'getData']);
 
 Route::post('/saveData', [AppController::class, 'saveData']);
 
 Route::post('/editData', [AppController::class, 'editData']);
-
-Route::middleware('auth:sanctum')->get('api/login', [AppController::class, 'login']);
